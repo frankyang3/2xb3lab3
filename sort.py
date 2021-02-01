7c12c11fa74ad3c1454c1f648409776d0d729fb8
Yi Luo <yiluo@Yis-Mac-mini.local>
2021-02-01 10:17:10 -0500

    quad

--- sort.py
+++ sort.py
@@ -60,53 +60,41 @@ def tri_pivot_quicksort_copy(L):
         x.sort()
         pivot1, pivot2, pivot3 = x[0], x[1], x[2]
         left1, left2, right1, right2 = [], [], [], []
-        for num in L[2:]:
+        for num in L[3:]:
                 if num < pivot1:
                         left1.append(num)
-                elif pivot1 <= num <= pivot2:
+                elif pivot1 <= num < pivot2:
                         left2.append(num)
-                elif pivot2 < num < pivot3:
+                elif pivot2 <= num < pivot3:
                         right1.append(num)
                 else:
                         right2.append(num)
         return tri_pivot_quicksort_copy(left1) + [pivot1] + tri_pivot_quicksort_copy(left2) + [pivot2] + tri_pivot_quicksort_copy(right1) + [pivot3] + tri_pivot_quicksort_copy(right2)
 
-def quad_quicksort(L):
-    copy = quicksort_copy_quad(L)
+def quad_pivot_quicksort(L):
+    copy = quad_pivot_quicksort_copy(L)
     for i in range(len(L)):
         L[i] = copy[i]
 
 
-def quicksort_copy_quad(L):
-    if len(L) < 1:
-        return []
-    elif len(L) < 2:
-        return L
-    elif len(L) < 3:
-        return [max(L[0], L[1]), min (L[0], L[1])]
-    elif len(L) < 4:
-        list.sort(L)
-        return L
-        
-    pivotList = [L[0],L[1], L[2], L[3]]
-    pivotList.sort()
-    pivot1 = pivotList[0]
-    pivot2 = pivotList[1]
-    pivot3 = pivotList[2]
-    pivot4 = pivotList[3]
-
-    left, midleft, mid, midright, right = [], [], [], [], []
-    for num in L[1:]:
-        if num <= pivot1:
-            left.append(num)
-        elif num <= pivot2 and num > pivot1:
-            midleft.append(num)
-        elif num <= pivot3 and num > pivot2:
-            mid.append(num)
-        elif num <= pivot4 and num > pivot3:
-            midright.append(num)
-        elif num > pivot4:
-            right.append(num)
-
-
-    return quicksort_copy_quad(left) + [pivot1] + quicksort_copy_quad(midleft) + [pivot2] + quicksort_copy_quad(mid) + [pivot3] + quicksort_copy_quad(midright) + [pivot4] + quicksort_copy_quad(right)
+def quad_pivot_quicksort_copy(L):
+        if len(L) < 5:
+                copy = L
+                copy.sort()
+                return copy
+        x = [L[0], L[1], L[2],L[3]]
+        x.sort()
+        pivot1, pivot2, pivot3, pivot4 = x[0], x[1], x[2], L[3]
+        left1, left2, right1, right2, right3 = [], [], [], [], []
+        for num in L[4:]:
+                if num < pivot1:
+                        left1.append(num)
+                elif pivot1 <= num < pivot2:
+                        left2.append(num)
+                elif pivot2 <= num < pivot3:
+                        right1.append(num)
+                elif pivot3 <= num < pivot4:
+                        right2.append(num)
+                else:
+                        right3.append(num)
+        return quad_pivot_quicksort_copy(left1) + [pivot1] + quad_pivot_quicksort_copy(left2) + [pivot2] + quad_pivot_quicksort_copy(right1) + [pivot3] + quad_pivot_quicksort_copy(right2) + [pivot4]+ quad_pivot_quicksort_copy(right3)
